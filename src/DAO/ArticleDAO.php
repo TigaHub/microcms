1808ef58bb1c4f5b06a95c81a90f4d0ee7c4a216
agiTed <xvr.dev@gmail.com>
2015-04-23 03:22:40 +0200

    Itération 10 - Back-office Admini

--- src/DAO/ArticleDAO.php
+++ src/DAO/ArticleDAO.php
@@ -25,20 +25,6 @@ class ArticleDAO extends DAO
     }
 
     /**
-     * Creates an Article object based on a DB row.
-     *
-     * @param array $row The DB row containing Article data.
-     * @return \MicroCMS\Domain\Article
-     */
-    protected function buildDomainObject($row) {
-        $article = new Article();
-        $article->setId($row['art_id']);
-        $article->setTitle($row['art_title']);
-        $article->setContent($row['art_content']);
-        return $article;
-    }
-
-    /**
      * Returns an article matching the supplied id.
      *
      * @param integer $id
@@ -53,5 +39,52 @@ class ArticleDAO extends DAO
             return $this->buildDomainObject($row);
         else
             throw new \Exception("No article matching id " . $id);
+    }
+
+    /**
+     * Saves an article into the database.
+     *
+     * @param \MicroCMS\Domain\Article $article The article to save
+     */
+    public function save(Article $article) {
+        $articleData = array(
+            'art_title' => $article->getTitle(),
+            'art_content' => $article->getContent(),
+            );
+
+        if ($article->getId()) {
+            // The article has already been saved : update it
+            $this->getDb()->update('t_article', $articleData, array('art_id' => $article->getId()));
+        } else {
+            // The article has never been saved : insert it
+            $this->getDb()->insert('t_article', $articleData);
+            // Get the id of the newly created article and set it on the entity.
+            $id = $this->getDb()->lastInsertId();
+            $article->setId($id);
+        }
+    }
+
+    /**
+     * Removes an article from the database.
+     *
+     * @param integer $id The article id.
+     */
+    public function delete($id) {
+        // Delete the article
+        $this->getDb()->delete('t_article', array('art_id' => $id));
+    }
+
+    /**
+     * Creates an Article object based on a DB row.
+     *
+     * @param array $row The DB row containing Article data.
+     * @return \MicroCMS\Domain\Article
+     */
+    protected function buildDomainObject($row) {
+        $article = new Article();
+        $article->setId($row['art_id']);
+        $article->setTitle($row['art_title']);
+        $article->setContent($row['art_content']);
+        return $article;
     }
 }
